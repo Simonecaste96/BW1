44c712a135e863c133194bdad1d5ea2e6069db11
Brokins00 <brokins2000@gmail.com>
2024-02-12 18:45:21 +0100

    Fix + pulizia codice

--- BW1/assets/js/script.js
+++ BW1/assets/js/script.js
@@ -1,24 +1,24 @@
 const gestioneCheckbox = () => {
-    const unchecked = document.getElementById('unchecked');
-    const checked = document.getElementById('checked');
-    const button = document.getElementById('button');
-    const avviso = document.querySelector('.avviso')
-    unchecked.addEventListener('click', (e) => {
-        unchecked.style.display = 'none';
-        checked.style.display = 'inline';
-        button.classList.add('abilitato')
-        avviso.style.display = 'none'
-    })
-    checked.addEventListener('click', (e) => {
-        unchecked.style.display = 'inline';
-        checked.style.display = 'none';
-        button.classList.remove('abilitato')
-        avviso.style.display = 'block'
-    })
-}
+    const unchecked = document.getElementById("unchecked");
+    const checked = document.getElementById("checked");
+    const button = document.getElementById("button");
+    const avviso = document.querySelector(".avviso");
+    unchecked.addEventListener("click", (e) => {
+        unchecked.style.display = "none";
+        checked.style.display = "inline";
+        button.classList.add("abilitato");
+        avviso.style.display = "none";
+    });
+    checked.addEventListener("click", (e) => {
+        unchecked.style.display = "inline";
+        checked.style.display = "none";
+        button.classList.remove("abilitato");
+        avviso.style.display = "block";
+    });
+};
 
 const init = () => {
-    gestioneCheckbox()
-}
+    gestioneCheckbox();
+};
 
-window.addEventListener('load', init)
+window.addEventListener("load", init);
